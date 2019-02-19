a462e522a33931cc01e1ab48e9cd3be852e98713
Alexander Mieth <alexmieth@alexmieth-macbook.dyndns.cern.ch>
2019-02-19 12:46:30 +0100

    level4_script now writes correct root input files to each cfg file generated.

--- code/HiggsExample20112012/Level4/level4_script.py
+++ code/HiggsExample20112012/Level4/level4_script.py
@@ -7,7 +7,7 @@ datasets = glob.glob('datasets/*.txt', recursive=True)
 with open('code/HiggsExample20112012/Level4/default_cfg.py', 'r') as default_cfg:
     default_content = default_cfg.read()
 
-N = 100
+N = 10
 count = 0
 #Iterate through each of these files
 for filename in datasets:
@@ -16,7 +16,11 @@ for filename in datasets:
         lines = fi.read().splitlines()
 
         for i in range(0, len(lines), N):
+            #Create list of N root files
             N_rootfiles = lines[i:i+N]
+
+            #Turn this list of N root files into one long string
+            new_input_filenames = ' '.join(N_rootfiles)
 
             count += 1
             new_filename = 'code/HiggsExample20112012/Level4/cfg_files/demoanalyzer_cfg' + str(count) + '.py'
@@ -25,6 +29,9 @@ for filename in datasets:
             new_content = default_content
 
             new_content = new_content.replace('OUTPUT_FILE_STR', new_output_filename)
+            new_content = new_content.replace('INPUT_FILES_STR', new_input_filenames)
+
+
 
             with open(new_filename,"w") as new_cfg:
                 new_cfg.write(new_content)
