78c3a754bd644585160c36add0a82dace6a33351
Alexander Mieth <alexander.hardy.mieth@cern.ch>
2019-03-27 17:45:56 +0100

    Adjusted exec scripts to run correctly within new folder organization.

--- batch_files/exec_combine.sh
+++ batch_files/exec_combine.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
+cd ../../
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd CMSSW_5_3_32/src
 eval `scramv1 runtime -sh`
-cd copy-h4l-demo/code/HiggsExample20112012/Level3
+cd reana-demo-cms-h4l/Level3/
 root -b -l -q ./M4Lnormdatall_lvl3.cc
--- batch_files/exec_mc.sh
+++ batch_files/exec_mc.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+cd ../
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd CMSSW_5_3_32/src
 eval `scramv1 runtime -sh`
 scram b
-cd copy-h4l-demo/code/HiggsExample20112012/Level3
+cd Level3/
 cmsRun demoanalyzer_cfg_level3MC.py
