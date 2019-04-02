76bba47e2b3d5788c9e9761b4b93e5ab5c361a62
Alexander Mieth <amieth74@gmail.com>
2019-04-02 05:13:37 +0200

    Adjusted exec files so that you can submit jobs within batch_files folder.

--- batch_files/exec_combine.sh
+++ batch_files/exec_combine.sh
@@ -3,5 +3,5 @@
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd CMSSW_5_3_32/src
 eval `scramv1 runtime -sh`
-cd copy-h4l-demo/code/HiggsExample20112012/Level3
+cd reana-demo-cms-h4l/code/HiggsExample20112012/Level3
 root -b -l -q ./M4Lnormdatall_lvl3.cc
