96b363ef4f69722651758de14b1250359561a112
Alexander Mieth <amieth74@gmail.com>
2019-04-03 13:41:42 +0200

    Removed outdated file.

--- batch_files/lvl4_singularity_cmd.sh
+++
@@ -1,5 +0,0 @@
-export SINGULARITY_CACHEDIR="/tmp/$(whoami)/singularity"
-echo "exported SINGULARITY_CACHEDIR"
-singularity exec -B /eos -B /cvmfs docker://clelange/slc6-cms:latest ./exec_lvl4_stage1.sh $1
-echo "Ran singularity cmd with argument:"
-echo $1
