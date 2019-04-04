83aad47f74757b7413403c096f58f0206811cc84
Alexander Mieth <amieth74@gmail.com>
2019-04-04 16:33:16 +0200

    Fixed typo in Level4 exec file. Level 4 runs fine now.

--- batch_files/exec_lvl4_stage1.sh
+++ batch_files/exec_lvl4_stage1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd CMSSW_5_3_32/src/
-eval 'scramv1 runtime -sh'
+cd CMSSW_5_3_32/src
+eval `scramv1 runtime -sh`
 scram b
 cd reana-demo-cms-h4l/code/HiggsExample20112012/Level4
 cmsRun ./cfg_files/demoanalyzer_cfg$1.py
